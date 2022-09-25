5a81833eda8cdbd0c22c9395b39fb0bb7406e82b
trashbin100 <114340005+trashbin100@users.noreply.github.com>
2022-09-25 06:36:51 -0500

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
-import streamlit 
+import streamlit as st
 
 streamlit.title("My Parents New Healthy Diner")
+
+st.header("Breakfast Menu")
+
+st.text("Omega 3 & Blueberry Oatmeal")
+
+st.text("Kale, Spinach & Rocket Smoothie")
+
+st.text("Hard-Boiled Free-Range Egg")
