8c5d3e4c2e2119609b9fdf45b9274f939945abad
trashbin100 <114340005+trashbin100@users.noreply.github.com>
2022-09-25 06:52:23 -0500

    Update streamlit_app.py
    moves the fruit list to the proper place

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-st.dataframe(my_fruit_list)
+
 
 
 st.title("My Parents New Healthy Diner")
@@ -20,3 +20,4 @@ st.text("🥑🍞 Avocado Toast")
 
 
 st.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
+st.dataframe(my_fruit_list)
