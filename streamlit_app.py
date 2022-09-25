1ff540ea55758e78ff4233d72e127b60b2231afa
trashbin100 <114340005+trashbin100@users.noreply.github.com>
2022-09-25 06:51:33 -0500

    Update streamlit_app.py
    imports pandas
    imports an online text file
    streamlit used to show the dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd 
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+st.dataframe(my_fruit_list)
+
 
 st.title("My Parents New Healthy Diner")
 
