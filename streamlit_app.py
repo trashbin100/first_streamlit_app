719c733fd2df48983e5f61ef1de58d6432bb4a59
trashbin100 <114340005+trashbin100@users.noreply.github.com>
2022-09-27 03:35:07 -0500

    Update streamlit_app.py
    Import requests and show text on the screen showing the API status.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
+import requests 
+
+
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
@@ -30,3 +33,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display on page
 st.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
