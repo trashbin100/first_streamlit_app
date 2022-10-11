fc2d885fd7178a355f1c9b2b08e9e0a610c62343
trashbin100 <114340005+trashbin100@users.noreply.github.com>
2022-10-11 10:22:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,8 +97,8 @@ try:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruit_choice)
     streamlit.dataframe(back_from_function)
-  finally:
-    pass
+  except URLError as e:
+    streamlit.error()
 
 st.header("Fruityvice Fruit Advice!")
 
