cab48ac0a852475d03c97d9e3bd0de0820558e0d
trashbin100 <114340005+trashbin100@users.noreply.github.com>
2022-10-11 09:09:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,23 @@ my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 #display on page
 st.dataframe(fruits_to_show)
 
+
+# New Section to display fruityvice api response 
+streamlit.header("Fruityvice Fruit Advice!")
+
+try:
+  fruit_choice = streamlit.text_input("What fruit would you like information about?")
+  if not fruit_choice:
+    streamlit.error("PLease select a fruit to get information")
+else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
+  
+  except URLError as e:
+    streamlit.error()
+
+
 # Enter a selection
 fruit_choice = st.text_input("What fruit would you like information about?", "Kiwi")
 streamlit.write("The user entered", fruit_choice)
@@ -65,14 +82,14 @@ streamlit.write("The user entered", fruit_choice)
 # Creates a variable for internet response 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 streamlit.text(fruityvice_response)
 
 
 # Make the JSON more Presentable 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # Showcases the API as a reading as an Entry 
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 
