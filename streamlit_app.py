97a0c3ed51b4277eace18dfe7957edd83a0f2fd3
trashbin100 <114340005+trashbin100@users.noreply.github.com>
2022-10-11 08:38:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 # Allow End User to add a fruit to the list 
-add_my_fruit = streamlit.selectbox(my_cnx)
+add_my_fruit = streamlit.selectbox(my_fruit_list)
 # Shows the new section of Fruityvice Response 
 streamlit.header("Fruityvice Fruit Advice!")
 #display on page
